a51885efd40c56edc624ae0b57e09be4f08be8ab
Nagaraja Jade <hello@build22.org>
2016-05-21 03:03:18 +0100

    use coordinators to perform concurrent processing

--- lib/metex.ex
+++ lib/metex.ex
@@ -1,2 +1,17 @@
 defmodule Metex do
+
+  def temperature_of(cities) do
+    # 1. Create a Coordinator process
+    coordinator_pid = spawn(Metex.Coordinator, :loop, [[], Enum.count(cities)])
+
+    # 2. Iterate through each city
+    cities |> Enum.each(
+    fn city ->
+      # 3. Create a worker process and execute its loop function
+      worker_pid = spawn(Metex.Worker, :loop, [])
+      # 4. Send the worker a message containing the coordinator pid and city
+      send worker_pid, {coordinator_pid, city}
+    end)
+  end
+
 end
